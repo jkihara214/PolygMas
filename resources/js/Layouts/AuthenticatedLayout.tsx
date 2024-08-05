2908a178985b339e4f12d136cfc347dfc37373ca
jkihara214 <jkihara214@gmail.com>
2024-08-05 14:50:20 +0900

    [Add app icon & name][] アプリのアイコン、名前を追加 <BLANK LINE> <body>

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -24,7 +24,7 @@ export default function Authenticated({
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationLogo className="block h-16 w-auto fill-current text-gray-800" />
                                 </Link>
                             </div>
 

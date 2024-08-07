eb086364b9f3f8d433bfa11857653dcecd8f46d0
jkihara214 <jkihara214@gmail.com>
2024-08-07 14:02:59 +0900

    [Add admin pass reset func][admin] 管理者画面に管理者パスワードリセット機能を追加 <BLANK LINE> <body>

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,10 +6,17 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Notifications\AdminResetPassword as ResetPasswordNotification;
 
 class Admin extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
+
+    public function sendPasswordResetNotification($token)
+    {
+
+        $this->notify(new ResetPasswordNotification($token));
+    }
 
     /**
      * The attributes that are mass assignable.
---
+++ tests/Feature/Http/Controllers/Admin/Auth/PasswordResetLinkControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Admin\Auth;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Admin;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Auth\Notifications\AdminResetPassword as ResetPasswordNotification;
+use Tests\TestCase;
+
+class PasswordResetLinkControllerTest extends TestCase
+{
+    // use RefreshDatabase;
+
+    public function test_admin_reset_password_link_screen_can_be_rendered(): void
+    {
+        $response = $this->get('/admin/forgot-password');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_admin_reset_password_link_can_be_requested(): void
+    {
+        Notification::fake();
+
+        $admin = Admin::factory()->create();
+
+        $this->post('/admin/forgot-password', ['email' => $admin->email]);
+
+        Notification::assertSentTo($admin, ResetPasswordNotification::class);
+    }
+
+    public function test_admin_reset_password_screen_can_be_rendered(): void
+    {
+        Notification::fake();
+
+        $admin = Admin::factory()->create();
+
+        $this->post('/admin/forgot-password', ['email' => $admin->email]);
+
+        Notification::assertSentTo($admin, ResetPasswordNotification::class, function ($notification) {
+            $response = $this->get('/admin/reset-password/' . $notification->token);
+
+            $response->assertStatus(200);
+
+            return true;
+        });
+    }
+
+    public function test_admin_password_can_be_reset_with_valid_token(): void
+    {
+        Notification::fake();
+
+        $admin = Admin::factory()->create();
+
+        $this->post('/admin/forgot-password', ['email' => $admin->email]);
+
+        Notification::assertSentTo($admin, ResetPasswordNotification::class, function ($notification) use ($admin) {
+            $response = $this->post('/admin/reset-password', [
+                'token' => $notification->token,
+                'email' => $admin->email,
+                'password' => 'password',
+                'password_confirmation' => 'password',
+            ]);
+
+            $response
+                ->assertSessionHasNoErrors()
+                ->assertRedirect(route('admin.login'));
+
+            return true;
+        });
+    }
+}
